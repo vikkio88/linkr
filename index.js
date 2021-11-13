c71cf966487a84313529d7264a0b881562224bd6
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-11-13 15:37:07 +0000

    adding version

--- index.js
+++ index.js
@@ -5,41 +5,46 @@ const { ulid } = require("ulid");
 
 const db = require("./db");
 
-const r404 = (_,res) => send(res,404);
+const r404 = (_, res) => send(res, 404);
 
 module.exports = cors(router(
-    get("/ping", (_,res) => send(res,200, {pong:true})),
+    get("/ping", (_, res) => send(res, 200, {
+        version: revision = require('child_process')
+            .execSync('git rev-parse HEAD')
+            .toString().trim(),
+        pong: true
+    })),
 
-    get("/links", async (_,res) => {
+    get("/links", async (_, res) => {
         const results = await db.find();
 
-        send(res,200, results);
+        send(res, 200, results);
     }),
-    post("/links", async (req,res) => {
+    post("/links", async (req, res) => {
         let body = null;
         try {
             body = await json(req);
             if (!body.link) throw Error("I cant even");
-        } catch(error) {
+        } catch (error) {
             console.error(error);
-            return send(res, 422, {error: "nope"});
+            return send(res, 422, { error: "nope" });
         }
-        await db.insert({_id: ulid(), link: body.link});
+        await db.insert({ _id: ulid(), link: body.link });
         const results = await db.find();
-        
+
         return send(res, 200, results);
     }),
 
-    del("/links/:id", async (req,res) => {
-        const {id} = req.params;
+    del("/links/:id", async (req, res) => {
+        const { id } = req.params;
         try {
-            
-            const row = await db.find({_id:id});
-            if(row) row.removeOne(0);
 
-        }catch (error) {
+            const row = await db.find({ _id: id });
+            if (row) row.removeOne(0);
+
+        } catch (error) {
             console.error(error);
-            return send(res,422, {error:"nope"});
+            return send(res, 422, { error: "nope" });
         }
         return send(res, 200);
     }),
